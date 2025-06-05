3471deb794c845ad727adb24c9b611899f40b749
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-06-05 15:20:16 +0200

    card pick animation done

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,20 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+
+  ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
-  constructor(){}
+export class GameComponent implements OnInit {
   count5 = [1,2,3,4,5];
   pickCardAnimation = false;
+  currentCard: string = '';
+  playedCards: string = '';
+  game: Game = new Game;
+
+  constructor(){
+  }
+
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+    
+  }
 
   pickCard() {
-    this.pickCardAnimation = true;
+    if(!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop() as string;
+      this.pickCardAnimation = true;
+      
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 }
 
